cce830c43f45e1ecd10a2aa51a03fdfd09d158c9
Shubham <shubhamlad1001@gmail.com>
2020-09-30 16:11:56 +0530

    fixed links

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-$('.scroll').on('click', function (e) {
-    e.preventDefault();
-    var offset = 0;
-    var target = this.hash;
-    if ($(this).data('offset') != undefined) offset = $(this).data('offset');
-    $('html, body').stop().animate({
-        'scrollTop': $(target).offset().top - offset
-    }, 500, 'swing', function () {
+$(".scroll").on("click", function (e) {
+  e.preventDefault();
+  var offset = 0;
+  var target = this.hash;
+  if ($(this).data("offset") != undefined) offset = $(this).data("offset");
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(target).offset().top - offset,
+      },
+      500,
+      "swing",
+      function () {
         // window.location.hash = target;
-    });
+      }
+    );
 });
 
 $(document).ready(function () {
-    $(".navbar-toggle").on("click", function () {
-        $(this).toggleClass("active");
-    });
+  $(".navbar-toggle").on("click", function () {
+    $(this).toggleClass("active");
+  });
 });
